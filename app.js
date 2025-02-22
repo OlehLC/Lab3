1b6b92c234c8b195f7ff88165b2ae639e0560c33
OlehLC <o.lobko2018@gmail.com>
2025-02-22 19:27:48 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+// const booksRouter = require("./routes/books");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
